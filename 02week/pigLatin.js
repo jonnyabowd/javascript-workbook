3e58f9c5d8c782f6f7f0d7737544acd3b430f41c
jonnyabowd <hello@jonnyabowd.com>
2019-03-04 23:35:22 -0600

    added wk 2 homework and RPS unit test

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -8,12 +8,45 @@ const rl = readline.createInterface({
 });
 
 
-function pigLatin(word) {
+function pigLatin(a) {
 
-  // Your code here
+  //your code here
+  a = a.toLowerCase().trim();
+  // convert word to array
+  const splitWord = a.split("");
 
+  //take vowels and see if first letter in array includes any
+  if(["a","e","i","o","u"].includes(splitWord[0])){
+    //if first letter includes a vowel run the following code
+    console.log("\"" + a + "\"" + " begins with a vowel!");
+    console.log("The way to say this in Pig Latin is:");
+    //log joined array into a single string and add 'ay' to the end
+    console.log("\"" + splitWord.join('') + "yay" + "\"");
+  } else {
+    //first letter DOES NOT include a vowel run the following code
+    console.log("\"" + a + "\"" + " does NOT start with a vowel!");
+    console.log("The way to say this in Pig Latin is:");
+    //begin 'for loop' to cycle through characters/array until we hit a vowel
+    for (let i = 0; i < splitWord.length; i++){
+
+        //add (push) first letter in array[0] to end of array
+        splitWord.push(splitWord[0]);
+
+        //remove first letter (shift) in the array
+        splitWord.shift();
+
+      //if we hit a vowel as we're cycling
+      if (["a","e","i","o","u"].includes(splitWord[0])){
+        //log joined array into a single string and add 'ay' to the end
+        console.log("\"" + splitWord.join('') + "ay" + "\"");
+        //then break out of the 'for loop'
+        break;
+      }
+
+    }
+  
+  }
 }
-
 
 function getPrompt() {
   rl.question('word ', (answer) => {

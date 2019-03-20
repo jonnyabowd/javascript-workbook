0e2230d89f9cff8a275dae6de707d1adfbea6df1
jonnyabowd <hello@jonnyabowd.com>
2019-03-19 21:36:34 -0400

    initial commit

--- 05week/spaceTravelToMars.js
+++ 05week/spaceTravelToMars.js
@@ -10,6 +10,26 @@ let jobTypes = {
 };
 
 // Your code here
+class CrewMember {
+  constructor(name, job, specialSkill, ship) {
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship = ship;
+  };
+}
+
+class Ship {
+  constructor(name, type, ability, crew) {
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = crew;
+  };
+}
+
+
+
 
 //tests
 if (typeof describe === 'function'){

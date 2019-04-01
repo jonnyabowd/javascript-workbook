74fadc7c56a932a36d732adc1fbcfb0bb1702845
jonnyabowd <43694246+jonnyabowd@users.noreply.github.com>
2019-03-31 23:31:53 -0500

    Git is not working tonight, again. FML
    fatal: unable to access 'https://github.com/jonnyabowd/javascript-workbook.git/': The requested URL returned error: 403

--- 05week/checkers.js
+++ 05week/checkers.js
@@ -9,167 +9,54 @@ const rl = readline.createInterface({
 
 
 class Checker {
-  // Your code here
-  constructor(color) {
-    if (color === 'red') {
-      this.symbol = 'R'
-      // this.symbol = String.fromCharCode(0x25CB);
-    } else if (color === 'noPlay') {
-      this.symbol = String.fromCharCode(0x2733);
-    } else {
-      this.symbol = 'B'
-      // this.symbol = String.fromCharCode(0x25CF);
+  // this is the method where we are taking in the checker type and coordinates
+  constructor(symbol, row, col) {
+    this.symbol = symbol;
+    this.row = row;
+    this.col = col;
+    this.isKing = false;
+  }
+  // if we call the makeKing function convert this.symbol's lowercase r to uppercase else lowercase b to uppercase
+  makeKing() {
+    if (!this.isKing) {
+      this.isKing = true;
+      if (this.symbol === "r") {
+        this.symbol = "R";
+      } else {
+        this.symbol = "B";
+      }
     }
   }
 }
 
-// ORIGINAL BOARD CLASS CODE BELOW FOR WHEN YOU SCREW SOMETHING UP
-// class Board {
-//   constructor() {
-//     this.grid = []
-//   }
-//   // method that creates an 8x8 array, filled with null values
-//   createGrid() {
-//     // loop to create the 8 rows
-//     for (let row = 0; row < 8; row++) {
-//       this.grid[row] = [];
-//       // push in 8 columns of nulls
-//       for (let column = 0; column < 8; column++) {
-//         this.grid[row].push(null);
-//       }
-//     }
-//   }
-//   viewGrid() {
-//     // add our column numbers
-//     let string = "  0 1 2 3 4 5 6 7\n";
-//     for (let row = 0; row < 8; row++) {
-//       // we start with our row number in our array
-//       const rowOfCheckers = [row];
-//       // a loop within a loop
-//       for (let column = 0; column < 8; column++) {
-//         // if the location is "truthy" (contains a checker piece, in this case)
-//         if (this.grid[row][column]) {
-//           // push the symbol of the check in that location into the array
-//           rowOfCheckers.push(this.grid[row][column].symbol);
-//         } else {
-//           // just push in a blank space
-//           rowOfCheckers.push(' ');
-//         }
-//       }
-//       // join the rowOfCheckers array to a string, separated by a space
-//       string += rowOfCheckers.join(' ');
-//       // add a 'new line'
-//       string += "\n";
-//     }
-//     console.log(string);
-//   }
-
-//   // Your code here
-// }
 
 class Board {
   constructor() {
-    this.checkers = [];
-    this.grid = [];
+    this.grid = []
+    this.checkers = []
   }
-
-  setBoard() {
-
-    // create a variable equal to an array inside an array with all the RED pieces in their starting positions
-    let redPositions = [
-      [0, 1], [0, 3], [0, 5], [0, 7],
-      [1, 0], [1, 2], [1, 4], [1, 6],
-      [2, 1], [2, 3], [2, 5], [2, 7]
-    ];
-
-    // use a for loop to iterate the amount of RED pieces we have
-    // each time the loop runs do the following:
-      // create a variable equal to array i position and the FIRST number in that nested array - this will tell us what ROW we're on
-      // create a variable equal to array i position and the SECOND number in that nested array - this will tell us what COLUMN we're on
-      // create a variable named RED equal to a new instance of the checker class passing in the color constructor as a string
-      // grab the checkers constructor (array) in the Board class and push the RED variable
-      // grab the grid constructor (array) at the specific row and column location and set it equal to the RED variable
-    for (let i =0; i < 12; i++) {
-      let redRow = redPositions[i][0];
-      let redColumn = redPositions[i][1];
-      let red = new Checker('red');
-      this.checkers.push(red);
-      this.grid[redRow][redColumn] = red;
-    };
-
-    // create a variable equal to an array inside an array with all the BLACK pieces in their starting positions
-    let blackPositions = [
-      [5, 0], [5, 2], [5, 4], [5, 6],
-      [6, 1], [6, 3], [6, 5], [6, 7],
-      [7, 0], [7, 2], [7, 4], [7, 6]
-    ];
-
-    // use a for loop to iterate the amount of BLACK pieces we have
-    // each time the loop runs do the following:
-      // create a variable equal to array i position and the FIRST number in that nested array - this will tell us what ROW we're on
-      // create a variable equal to array i position and the SECOND number in that nested array - this will tell us what COLUMN we're on
-      // create a variable named BLACK equal to a new instance of the checker class passing in the color constructor as a string
-      // grab the checkers constructor (array) in the Board class and push the BLACK variable
-      // grab the grid constructor (array) at the specific row and column location and set it equal to the BLACK variable
-    for (let i =0; i < 12; i++) {
-      let blackRow = blackPositions[i][0];
-      let blackColumn = blackPositions[i][1];
-      let black = new Checker('black');
-      this.checkers.push(black);
-      this.grid[blackRow][blackColumn] = black;
-    };
-
-    // create a variable equal to an array inside an array with all the places on the board users CANNOT PLAY INTO
-    let noPlaySpaces = [
-      [0, 0], [0, 2], [0, 4], [0, 6],
-      [1, 1], [1, 3], [1, 5], [1, 7],
-      [2, 0], [2, 2], [2, 4], [2, 6],
-      [3, 1], [3, 3], [3, 5], [3, 7],
-      [4, 0], [4, 2], [4, 4], [4, 6],
-      [5, 1], [5, 3], [5, 5], [5, 7],
-      [6, 0], [6, 2], [6, 4], [6, 6],
-      [7, 1], [7, 3], [7, 5], [7, 7],
-    ];
-
-    // use a for loop to iterate the amount of SPACES WE CANNOT PLAY that we have
-    // each time the loop runs do the following:
-      // create a variable equal to array i position and the FIRST number in that nested array - this will tell us what ROW we're on
-      // create a variable equal to array i position and the SECOND number in that nested array - this will tell us what COLUMN we're on
-      // create a variable named NO PLAY equal to a new instance of the checker class passing in the color constructor as a string
-      // grab the checkers constructor (array) in the Board class and push the NO PLAY variable
-      // grab the grid constructor (array) at the specific row and column location and set it equal to the NO PLAY variable
-    for (let i =0; i < 32; i++) {
-      let noPlayRow = noPlaySpaces[i][0];
-      let noPlayColumn = noPlaySpaces[i][1];
-      let noPlay = new Checker('noPlay');
-      this.checkers.push(noPlay);
-      this.grid[noPlayRow][noPlayColumn] = noPlay;
-    };
-
-  }
-
-  // method that creates an 8x8 array, filled with null values
+  // creates an 8x8 array, filled with null values
   createGrid() {
-    // loop to create the 8 rows
+    // loop to create the 8 rows of null values
     for (let row = 0; row < 8; row++) {
       this.grid[row] = [];
-      // push in 8 columns of nulls
+      // push in 8 columns of null values
       for (let column = 0; column < 8; column++) {
         this.grid[row].push(null);
       }
     }
   }
   viewGrid() {
-    // add our column numbers
+    // adding column numbers
     let string = "  0 1 2 3 4 5 6 7\n";
     for (let row = 0; row < 8; row++) {
       // we start with our row number in our array
       const rowOfCheckers = [row];
       // a loop within a loop
       for (let column = 0; column < 8; column++) {
-        // if the location is "truthy" (contains a checker piece, in this case)
+        // if the location is "truthy" (contains a checker piece)
         if (this.grid[row][column]) {
-          // push the symbol of the check in that location into the array
+          // push the symbol of the checker in that location into the array
           rowOfCheckers.push(this.grid[row][column].symbol);
         } else {
           // just push in a blank space
@@ -184,22 +71,133 @@ class Board {
     console.log(string);
   }
 
-  // // FAILED ATTEMPT TO MANUALLY SET BOARD PIECES
-  // setBoard() {
+  // this function fills the board on initial setup
+  createCheckers() {
+    // create a for loop and run through every row
+    for (let row = 0; row < 8; row++) {
+      // create another for loop and run through every column within the row
+      for (let col = 0; col < 8; col++) {
+        // add the row and col together and use remainder operator (%) to divide by 2 and return the remainder. if there is a remainder of 1 && the row value is less than 3, push an 'r' into that coordinate of the array. 
+        if ((row + col) % 2 === 1 && row < 3) {
+          const newChecker = new Checker("r", row, col);
+          this.grid[row][col] = newChecker;
+          this.checkers.push(newChecker);
+        } 
+        // add the row and col together and use remainder operator (%) to divide by 2 and return the remainder. if there is a remainder of 1 && the row value is less than 4, push a 'b' into that coordinate of the array.
+        else if ((row + col) % 2 === 1 && row > 4) {
+          const newChecker = new Checker("b", row, col);
+          this.grid[row][col] = newChecker;
+          this.checkers.push(newChecker);
+        } 
+        // add the row and col together and use remainder operator (%) to divide by 2 and return the remainder. if there is a remainder of 0 this is a non-playable space. Push a character into that coordinate of the array.        
+        else if ((row + col) % 2 === 0) {
+          const newChecker = new Checker(String.fromCharCode(0x2733), row, col);
+          this.grid[row][col] = newChecker;
+          this.checkers.push(newChecker);
+        }
+      }
+    }
+  }
 
-  //   this.grid[0,0] = [null,'R',null,'R',null,'R',null,'R'];
-  //   this.grid[0,1] = ['R',null,'R',null,'R',null,'R',null];
-  //   this.grid[0,2] = [null,'R',null,'R',null,'R',null,'R'];
+  //
+  findPiece(coordinate) {
+    const row = coordinate[0];
+    const col = coordinate[1];
+    const currentPiece = this.checkers.find(checker => {
+      return checker.row === row && checker.col === col;
+    });
+    return currentPiece;
+  }
 
-  //   console.log(this.grid);
-    
-  //   // for (let row = 0; row < 8; row++) {
-  //   //   if (this.grid[row] % 2 === 0) {
-  //   //     rowOfCheckers.push('+');
-  //   //   }
-  //   // }
-  // }
+  // this function receives the currentPiece coordinates and uses them to set that space on the grid equal to null and removes it from the checkers array
+  removePiece(currentPiece) {
+    const row = currentPiece.row;
+    const col = currentPiece.col;
+    this.grid[row][col] = null;
+    const index = this.checkers.indexOf(currentPiece);
+    this.checkers.splice(index, 1);
+  }
+
+  /* 
+  this function checks to make sure the move is legal by taking in the current and the destination coordinates then: 
+  - making sure the destination space is empty
+  - making sure the piece is going in the right direction
+  - making sure the piece is going the correct distance (move = 1 or jump = 2) 
+  */
+  isLegalMove(currentPiece, destination) {
+    const newRow = destination[0];
+    const newCol = destination[1];
+    const oldRow = currentPiece.row;
+    const oldCol = currentPiece.col;
+    if (this.grid[newRow][newCol]) {
+      return false;
+    }
+    if (currentPiece.symbol === "r" || currentPiece.isKing) {
+      if (newRow === oldRow + 1 && (newCol === oldCol + 1 || newCol === oldCol - 1)) {
+        return true;
+      } else if (newRow === oldRow + 2 && newCol === oldCol + 2 &&
+        this.grid[oldRow + 1][oldCol + 1]) {
+        this.removePiece(this.grid[oldRow + 1][oldCol + 1]);
+        return true;
+      } else if (newRow === oldRow + 2 && newCol === oldCol - 2 &&
+        this.grid[oldRow + 1][oldCol - 1]) {
+        this.removePiece(this.grid[oldRow + 1][oldCol - 1]);
+        return true;
+      }
+    }
+    if (currentPiece.symbol === "b" || currentPiece.isKing) {
+      if (newRow === oldRow - 1 && (newCol === oldCol + 1 || newCol === oldCol - 1)) {
+        return true;
+      } else if (newRow === oldRow - 2 && newCol === oldCol + 2 &&
+        this.grid[oldRow - 1][oldCol + 1]) {
+        this.removePiece(this.grid[oldRow - 1][oldCol + 1]);
+        return true;
+      } else if (newRow === oldRow - 2 && newCol === oldCol - 2 &&
+        this.grid[oldRow - 1][oldCol - 1]) {
+        this.removePiece(this.grid[oldRow - 1][oldCol - 1]);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // take in the origin and destination coordinates and move the piece
+  movePiece(currentPiece, destination) {
+    const newRow = destination[0];
+    const newCol = destination[1];
+    const oldRow = currentPiece.row;
+    const oldCol = currentPiece.col;
+    // in the grid find the new space and set it equal to the currentPiece and set the old space equal to null
+    this.grid[newRow][newCol] = currentPiece;
+    this.grid[oldRow][oldCol] = null;
+    currentPiece.row = newRow;
+    currentPiece.col = newCol;
+    // once a piece is moved to either the very top or very bottom row run the makeKing function on the currentPiece
+    if (newRow === 7 || newRow === 0) {
+      currentPiece.makeKing();
+    }
+  }
+
+  // this function checks for win by searching the entire checkers array to see if it contains at least one piece from either player
+  checkForWin() {
+    const containAnR = this.checkers.some(checker => {
+      return checker.symbol === "r" || checker.symbol === "R";
+    });
+    if (!containAnR) {
+      console.log("RED Wins!");
+      return true;
+    }
+    const containAnB = this.checkers.some(checker => {
+      return checker.symbol === "b" || checker.symbol === "B";
+    });
+    if (!containAnB) {
+      console.log("BLACK Wins!");
+      return true;
+    }
+    return false;
+  }
 }
+
 
 class Game {
   constructor() {
@@ -207,7 +205,46 @@ class Game {
   }
   start() {
     this.board.createGrid();
-    this.board.setBoard();
+    this.board.createCheckers();
+  }
+
+  // this function takes in the whichPiece and toWhere inputs
+  moveChecker(whichPiece, toWhere) {
+    // run the parseInput function on each input to check for valid 0-7 values
+    const origin = this.parseInput(whichPiece);
+    const destination = this.parseInput(toWhere);
+    // if either returned null tell them they stupid and try again
+    if (!origin || !destination) {
+      console.log("You silly! That is not a valid board location. Try again!");
+      return null;
+    }
+    // make sure they actually selected one of their pieces. Pass the origin array into the findPiece function
+    const currentPiece = this.board.findPiece(origin);
+    if (!currentPiece) {
+      console.log("No checker selected. Try again!");
+      return null;
+    }
+    // run the isLegalMove function and pass in currentPiece and destination coordinates
+    const isLegalMove = this.board.isLegalMove(currentPiece, destination);
+    if (!isLegalMove) {
+      console.log("Not a legal move! Try again!");
+      return null;
+    } else {
+      this.board.movePiece(currentPiece, destination);
+    }
+  }
+
+  // take the string input, return as integers, and makes sure they are valid 0-7 values
+  parseInput(string) {
+    const row = parseInt(string[0]);
+    const col = parseInt(string[1]);
+    const validInputs = [0, 1, 2, 3, 4, 5, 6, 7];
+    // if the row OR the col inputs to NOT include one of the valid inputs, return null
+    if (!validInputs.includes(row) || !validInputs.includes(col)) {
+      return null;
+    }
+    // return the input as an array
+    return [row, col];
   }
 }
 
@@ -222,37 +259,19 @@ function getPrompt() {
 }
 
 const game = new Game();
+// call the start function which will run the createGrid and createCheckers functions
 game.start();
 
 
-/*
-CHECKERS LOGIC
-- At start of game create 8x8 board
-  - Use a for loop to create a row array with 8 spaces
-  - Alternate placing a '+' and null in each space
-    - Create a const to hold if row should start with '+' and set it to true
-    - Start with an '+' if true, else start with null and iterate through the rest of the loop
-  - Create a for loop to run the row for loop 8 times to create 8 rows stacked on top of each other
 
-- Add row and column labels
-  - Create a row of 8 column numbers (0-7) separated by spaces (as a string)
-    - Add a space first to account for the following first column of row labels
-  - Using 2 for loops, create 8 additional rows with one row label followed by 8 open columns separated by spaces
-  - Use the first for loop to enter the row number then run the second for loop
 
-- Push the correct pieces and spaces to the board
-  -
 
-  9 -9 11 -11 for single jump
-  18 -18 22 -22 for double jump
 
-  - create function to check for valid move
-    - should see if whichPiece matches players turn
-  - create a function to move the piece
-    - run check for valid move function
-    
 
-*/
+
+
+
+
 
 
 // Tests
@@ -265,6 +284,7 @@ if (typeof describe === 'function') {
       assert.equal(game.board.checkers.length, 24);
     });
   });
+
   describe('Game.moveChecker()', () => {
     it('should move a checker', () => {
       assert(!game.board.grid[4][1]);
@@ -285,3 +305,10 @@ if (typeof describe === 'function') {
 } else {
   getPrompt();
 }
+
+/* 
+-create the board dimensions
+-lay all pieces on the board
+-be able to move pieces
+-be able to kill another piece
+*/
